f73609dc3af4ef87b16c570145babe2431ac99d3
dooms123321 <dmazariegosd4@miumg.edu.gt>
2026-04-16 11:13:49 -0600

    Subiendo cambios rama main

---
+++ Main/Capa_Vista_Logista/Frm_LOGIN.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace Capa_Vista_Logista
+{
+    partial class Frm_LOGIN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_LOGIN));
+            this.Lbl_Migracion = new System.Windows.Forms.Label();
+            this.Gbp_Campos = new System.Windows.Forms.GroupBox();
+            this.Chk_MostrarContrasena = new System.Windows.Forms.CheckBox();
+            this.Lbl_Contraseña = new System.Windows.Forms.Label();
+            this.Lbl_Usuario = new System.Windows.Forms.Label();
+            this.Btn_InicarSesion = new System.Windows.Forms.Button();
+            this.Txt_Contraseña = new System.Windows.Forms.TextBox();
+            this.Txt_Usuario = new System.Windows.Forms.TextBox();
+            this.Pcb_Imgane = new System.Windows.Forms.PictureBox();
+            this.Gbp_Campos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pcb_Imgane)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Lbl_Migracion
+            // 
+            this.Lbl_Migracion.AutoSize = true;
+            this.Lbl_Migracion.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Migracion.Location = new System.Drawing.Point(465, 53);
+            this.Lbl_Migracion.Name = "Lbl_Migracion";
+            this.Lbl_Migracion.Size = new System.Drawing.Size(92, 22);
+            this.Lbl_Migracion.TabIndex = 50;
+            this.Lbl_Migracion.Text = "Logística";
+            // 
+            // Gbp_Campos
+            // 
+            this.Gbp_Campos.Controls.Add(this.Chk_MostrarContrasena);
+            this.Gbp_Campos.Controls.Add(this.Lbl_Contraseña);
+            this.Gbp_Campos.Controls.Add(this.Lbl_Usuario);
+            this.Gbp_Campos.Controls.Add(this.Btn_InicarSesion);
+            this.Gbp_Campos.Controls.Add(this.Txt_Contraseña);
+            this.Gbp_Campos.Controls.Add(this.Txt_Usuario);
+            this.Gbp_Campos.Location = new System.Drawing.Point(104, 117);
+            this.Gbp_Campos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Gbp_Campos.Name = "Gbp_Campos";
+            this.Gbp_Campos.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Gbp_Campos.Size = new System.Drawing.Size(599, 338);
+            this.Gbp_Campos.TabIndex = 49;
+            this.Gbp_Campos.TabStop = false;
+            // 
+            // Chk_MostrarContrasena
+            // 
+            this.Chk_MostrarContrasena.AutoSize = true;
+            this.Chk_MostrarContrasena.Font = new System.Drawing.Font("Rockwell", 10F);
+            this.Chk_MostrarContrasena.Location = new System.Drawing.Point(436, 103);
+            this.Chk_MostrarContrasena.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Chk_MostrarContrasena.Name = "Chk_MostrarContrasena";
+            this.Chk_MostrarContrasena.Size = new System.Drawing.Size(118, 44);
+            this.Chk_MostrarContrasena.TabIndex = 46;
+            this.Chk_MostrarContrasena.Text = "mostrar\r\ncontraseña";
+            this.Chk_MostrarContrasena.UseVisualStyleBackColor = true;
+            this.Chk_MostrarContrasena.CheckedChanged += new System.EventHandler(this.Chk_MostrarContrasena_CheckedChanged);
+            // 
+            // Lbl_Contraseña
+            // 
+            this.Lbl_Contraseña.AutoSize = true;
+            this.Lbl_Contraseña.Font = new System.Drawing.Font("Rockwell", 10F);
+            this.Lbl_Contraseña.Location = new System.Drawing.Point(11, 114);
+            this.Lbl_Contraseña.Name = "Lbl_Contraseña";
+            this.Lbl_Contraseña.Size = new System.Drawing.Size(105, 20);
+            this.Lbl_Contraseña.TabIndex = 45;
+            this.Lbl_Contraseña.Text = "Contraseña:";
+            // 
+            // Lbl_Usuario
+            // 
+            this.Lbl_Usuario.AutoSize = true;
+            this.Lbl_Usuario.Font = new System.Drawing.Font("Rockwell", 10F);
+            this.Lbl_Usuario.Location = new System.Drawing.Point(40, 54);
+            this.Lbl_Usuario.Name = "Lbl_Usuario";
+            this.Lbl_Usuario.Size = new System.Drawing.Size(75, 20);
+            this.Lbl_Usuario.TabIndex = 44;
+            this.Lbl_Usuario.Text = "Usuario:";
+            // 
+            // Btn_InicarSesion
+            // 
+            this.Btn_InicarSesion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(78)))), ((int)(((byte)(88)))));
+            this.Btn_InicarSesion.FlatAppearance.BorderSize = 0;
+            this.Btn_InicarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_InicarSesion.Font = new System.Drawing.Font("Rockwell", 10F);
+            this.Btn_InicarSesion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(221)))));
+            this.Btn_InicarSesion.Location = new System.Drawing.Point(197, 202);
+            this.Btn_InicarSesion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_InicarSesion.Name = "Btn_InicarSesion";
+            this.Btn_InicarSesion.Size = new System.Drawing.Size(141, 48);
+            this.Btn_InicarSesion.TabIndex = 43;
+            this.Btn_InicarSesion.Text = "Iniciar Sesión";
+            this.Btn_InicarSesion.UseVisualStyleBackColor = false;
+            this.Btn_InicarSesion.Click += new System.EventHandler(this.Btn_InicarSesion_Click);
+            // 
+            // Txt_Contraseña
+            // 
+            this.Txt_Contraseña.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(236)))), ((int)(((byte)(238)))));
+            this.Txt_Contraseña.Font = new System.Drawing.Font("Rockwell", 10F);
+            this.Txt_Contraseña.Location = new System.Drawing.Point(137, 108);
+            this.Txt_Contraseña.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Txt_Contraseña.Name = "Txt_Contraseña";
+            this.Txt_Contraseña.Size = new System.Drawing.Size(293, 27);
+            this.Txt_Contraseña.TabIndex = 41;
+            this.Txt_Contraseña.UseSystemPasswordChar = true;
+            // 
+            // Txt_Usuario
+            // 
+            this.Txt_Usuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(236)))), ((int)(((byte)(238)))));
+            this.Txt_Usuario.Font = new System.Drawing.Font("Rockwell", 10F);
+            this.Txt_Usuario.Location = new System.Drawing.Point(137, 54);
+            this.Txt_Usuario.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Txt_Usuario.Name = "Txt_Usuario";
+            this.Txt_Usuario.Size = new System.Drawing.Size(293, 27);
+            this.Txt_Usuario.TabIndex = 40;
+            // 
+            // Pcb_Imgane
+            // 
+            this.Pcb_Imgane.Image = ((System.Drawing.Image)(resources.GetObject("Pcb_Imgane.Image")));
+            this.Pcb_Imgane.Location = new System.Drawing.Point(719, 135);
+            this.Pcb_Imgane.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Pcb_Imgane.Name = "Pcb_Imgane";
+            this.Pcb_Imgane.Size = new System.Drawing.Size(249, 299);
+            this.Pcb_Imgane.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Pcb_Imgane.TabIndex = 48;
+            this.Pcb_Imgane.TabStop = false;
+            // 
+            // Frm_LOGIN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.Lbl_Migracion);
+            this.Controls.Add(this.Gbp_Campos);
+            this.Controls.Add(this.Pcb_Imgane);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Frm_LOGIN";
+            this.Text = "Login";
+            this.Gbp_Campos.ResumeLayout(false);
+            this.Gbp_Campos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pcb_Imgane)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl_Migracion;
+        private System.Windows.Forms.GroupBox Gbp_Campos;
+        private System.Windows.Forms.CheckBox Chk_MostrarContrasena;
+        private System.Windows.Forms.Label Lbl_Contraseña;
+        private System.Windows.Forms.Label Lbl_Usuario;
+        private System.Windows.Forms.Button Btn_InicarSesion;
+        private System.Windows.Forms.TextBox Txt_Contraseña;
+        private System.Windows.Forms.TextBox Txt_Usuario;
+        private System.Windows.Forms.PictureBox Pcb_Imgane;
+    }
+}
---
+++ Main/Capa_Vista_Logista/Frm_MDI.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Capa_Vista_Logista
+{
+    partial class Frm_MDI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_MDI));
+            this.Pnl_Superior = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarSesiónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.catálogosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.procesosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.herramientasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reporteadorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asignacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seguridadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mantenimientosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Pnl_Superior.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Pnl_Superior
+            // 
+            this.Pnl_Superior.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(142)))), ((int)(((byte)(181)))));
+            this.Pnl_Superior.Controls.Add(this.label1);
+            this.Pnl_Superior.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Pnl_Superior.Location = new System.Drawing.Point(0, 0);
+            this.Pnl_Superior.Margin = new System.Windows.Forms.Padding(2);
+            this.Pnl_Superior.Name = "Pnl_Superior";
+            this.Pnl_Superior.Size = new System.Drawing.Size(829, 52);
+            this.Pnl_Superior.TabIndex = 99;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(21, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MDI bodegas";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(175)))), ((int)(((byte)(187)))));
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.catálogosToolStripMenuItem,
+            this.procesosToolStripMenuItem,
+            this.herramientasToolStripMenuItem,
+            this.asignacionesToolStripMenuItem,
+            this.seguridadToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 54);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 2, 6, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(587, 25);
+            this.menuStrip1.TabIndex = 101;
+            this.menuStrip1.Text = "MenuStrip";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cerrarSesiónToolStripMenuItem});
+            this.archivoToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(50, 21);
+            this.archivoToolStripMenuItem.Text = "Abrir";
+            // 
+            // cerrarSesiónToolStripMenuItem
+            // 
+            this.cerrarSesiónToolStripMenuItem.Name = "cerrarSesiónToolStripMenuItem";
+            this.cerrarSesiónToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cerrarSesiónToolStripMenuItem.Text = "Salir";
+            this.cerrarSesiónToolStripMenuItem.Click += new System.EventHandler(this.cerrarSesiónToolStripMenuItem_Click);
+            // 
+            // catálogosToolStripMenuItem
+            // 
+            this.catálogosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mantenimientosToolStripMenuItem});
+            this.catálogosToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.catálogosToolStripMenuItem.Name = "catálogosToolStripMenuItem";
+            this.catálogosToolStripMenuItem.Size = new System.Drawing.Size(83, 21);
+            this.catálogosToolStripMenuItem.Text = "Catálogos";
+            // 
+            // procesosToolStripMenuItem
+            // 
+            this.procesosToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.procesosToolStripMenuItem.Name = "procesosToolStripMenuItem";
+            this.procesosToolStripMenuItem.Size = new System.Drawing.Size(79, 21);
+            this.procesosToolStripMenuItem.Text = "Procesos";
+            // 
+            // herramientasToolStripMenuItem
+            // 
+            this.herramientasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reporteadorToolStripMenuItem});
+            this.herramientasToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.herramientasToolStripMenuItem.Name = "herramientasToolStripMenuItem";
+            this.herramientasToolStripMenuItem.Size = new System.Drawing.Size(74, 21);
+            this.herramientasToolStripMenuItem.Text = "Informes";
+            // 
+            // reporteadorToolStripMenuItem
+            // 
+            this.reporteadorToolStripMenuItem.Name = "reporteadorToolStripMenuItem";
+            this.reporteadorToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.reporteadorToolStripMenuItem.Text = "Reporteador";
+            this.reporteadorToolStripMenuItem.Click += new System.EventHandler(this.reporteadorToolStripMenuItem_Click);
+            // 
+            // asignacionesToolStripMenuItem
+            // 
+            this.asignacionesToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.asignacionesToolStripMenuItem.Name = "asignacionesToolStripMenuItem";
+            this.asignacionesToolStripMenuItem.Size = new System.Drawing.Size(67, 21);
+            this.asignacionesToolStripMenuItem.Text = "Ayudas";
+            // 
+            // seguridadToolStripMenuItem
+            // 
+            this.seguridadToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seguridadToolStripMenuItem.Name = "seguridadToolStripMenuItem";
+            this.seguridadToolStripMenuItem.Size = new System.Drawing.Size(100, 21);
+            this.seguridadToolStripMenuItem.Text = "Herramientas";
+            this.seguridadToolStripMenuItem.Click += new System.EventHandler(this.seguridadToolStripMenuItem_Click);
+            // 
+            // mantenimientosToolStripMenuItem
+            // 
+            this.mantenimientosToolStripMenuItem.Name = "mantenimientosToolStripMenuItem";
+            this.mantenimientosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.mantenimientosToolStripMenuItem.Text = "Mantenimientos";
+            this.mantenimientosToolStripMenuItem.Click += new System.EventHandler(this.mantenimientosToolStripMenuItem_Click);
+            // 
+            // Frm_MDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.Pnl_Superior);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Frm_MDI";
+            this.Text = "Mdi Logistica";
+            this.Load += new System.EventHandler(this.Frm_MDI_Load);
+            this.Pnl_Superior.ResumeLayout(false);
+            this.Pnl_Superior.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Pnl_Superior;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarSesiónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem catálogosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem procesosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem herramientasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reporteadorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asignacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seguridadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientosToolStripMenuItem;
+    }
+}
